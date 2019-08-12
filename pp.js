555093670e0d40170f82aa45602f40a7117eb8ea
madhukarjha <madhukar.jha@gmail.com>
2019-08-13 00:17:02 +0530

    state list

--- pp.js
+++ pp.js
@@ -1,19 +1,26 @@
 const puppeteer = require("puppeteer");
 const cheerio = require('cheerio');
 
-const scrapingResults = [
-    {
+// const scrapingResults = [
+//     {
      
-    }
-]
+//     }
+// ]
 
 async function main() {
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
-    await page.goto("https://www.google.com");
+    await page.goto("https://www.mypetrolprice.com/petrol-price-in-india.aspx");
     const html  = await page.content();
     const $ = cheerio.load(html);
-    
+    let states = [];
+    $('#StateDropDownList').find('option').each((i,op) => {
+        states.push($(op).text());
+    })
+    states.forEach(function(state){
+        console.log(state);
+    });
+  
 }
 
-main();
+main(); 
