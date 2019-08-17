5c0dd03ad20a2740af431761a591842c1f590ea4
madhukarjha <madhukar.jha@gmail.com>
2019-08-17 11:06:23 +0530

    get html

--- pp.js
+++ pp.js
@@ -1,26 +1,43 @@
 const puppeteer = require("puppeteer");
 const cheerio = require('cheerio');
-
-// const scrapingResults = [
-//     {
-     
-//     }
-// ]
+const request = require('request-promise');
 
 async function main() {
-    const browser = await puppeteer.launch({headless: true});
-    const page = await browser.newPage();
-    await page.goto("https://www.mypetrolprice.com/petrol-price-in-india.aspx");
-    const html  = await page.content();
-    const $ = cheerio.load(html);
+    const url = "https://www.mypetrolprice.com/petrol-price-in-india.aspx";
+    const html = await getHtml(url, false);
+    const $ = await cheerio.load(html);
     let states = [];
-    $('#StateDropDownList').find('option').each((i,op) => {
-        states.push($(op).text());
-    })
-    states.forEach(function(state){
-        console.log(state);
-    });
-  
+    $("h2.txtC+div div.W70, h2.txtC+div div.fl, h2.txtC+div div.fnt18").map((i, element) => {
+        console.log(element);
+    }).get();
+    // $('#StateDropDownList').find('option').each((i,op) => {
+    //     states.push($(op).text());
+    // })
+    // states.forEach(function(state){
+
+    //     console.log(state);
+    // });
+    // await page.waitFor(5000)
+    // await page.select('#StateDropDownList', states[1]);
+    // await page.waitFor(5000)
+    // const selectElem = await page.$('select[id="StateDropDownList"]');
+    // await selectElem.type(states[1]);
+    // await page.waitFor(5000)
+}
+
+async function getHtml(url, headless) {
+    let html = '';
+    if (headless) {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.goto(url);
+        html = await page.content(url);
+        return html;
+    }
+    else {
+        html = await request.get(url);
+        return html;
+    }
 }
 
 main(); 
